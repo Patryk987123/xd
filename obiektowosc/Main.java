a17034648b7b4a0c304701c5f6cee453530fb3dd
Patryk987123 <148445293+Patryk987123@users.noreply.github.com>
2024-10-03 20:25:16 +0200

    Update Main.java

--- obiektowosc/Main.java
+++ obiektowosc/Main.java
@@ -1,1 +1,11 @@
+ public static void main(String[] args) {
+   //zad1
+        Kwadrat kwadrat = new Kwadrat(9);
+   
+        System.out.println("Obwod:" + kwadrat.obwod());
+        System.out.println("Pole:" + kwadrat.pole());
+   //zad2
+   //zad3
+   //zad4
 
+    }
