76e6e5091c69051c6545451099e50acdf6d98e78
Patryk987123 <148445293+Patryk987123@users.noreply.github.com>
2024-10-03 21:50:47 +0200

    Update Main.java

--- dziedziczenie/Main.java
+++ dziedziczenie/Main.java
@@ -3,26 +3,25 @@ public class Main {
         //zad1
 Dog dog = new Dog("Burek","bialy");
 Cat cat = new Cat("Mruczek","brazowy");
-    System.out.println(dog.makeSound());
-    System.out.println(cat.makeSound());
+System.out.println(dog.makeSound());
+System.out.println(cat.makeSound());
         //zad2
-        E e = new E();
-    e.present("Super");
+E e = new E();
+e.present("Super");
         //zad3
 zad3 x = new zad3("x","y","xx",1,2);
-        System.out.println(x.toString());
+System.out.println(x.toString());
         // zad4
 String[] book={"x","y","z","c","v"};
 String[] bread={"x","y","z","c","v"};
-Bakery bakery = new Bakery("a",2,book);
-Bookshop bookshop = new Bookshop("a",2,bread);
+Bakery bakery=new Bakery("a",2,book);
+Bookshop bookshop=new Bookshop("a",2,bread);
 System.out.println(bakery.getInformation());
 System.out.println(bookshop.getInformation());
         //zad5
-        Calculator calculator = new Calculator();
-
-        System.out.println("2^2= " + calculator.calculate(2));        
-        System.out.println("2*3= " + calculator.calculate(2, 3)); 
-        System.out.println("1+2+3= " + calculator.calculate(1, 2, 3));
+Calculator calculator=new Calculator();
+System.out.println("Wynik:"+calculator.calculate(2));        
+System.out.println("Wynik:"+calculator.calculate(2,3)); 
+System.out.println("Wynik:"+calculator.calculate(1,2,3));
     }
 }
