39104d8eaecc3d0bb4f9c3a960db6b4b5f874dfd
Patryk987123 <148445293+Patryk987123@users.noreply.github.com>
2024-10-03 19:58:57 +0200

    Delete Cat.java

--- Cat.java
+++
@@ -1,9 +0,0 @@
-public class Cat extends Animal{
-    public Cat(String name, String color) {
-        super(name, color);
-    }
-
-    public String makeSound(){
-        return "Sound:HauHau"+getinfo();
-    }
-}
